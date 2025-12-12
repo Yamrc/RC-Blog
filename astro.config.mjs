4655a64f1ec356f88037b8f555233072b4666ebf
Yamrc <yamrci@outlook.com>
2025-12-13 03:17:15 +0800

    Feat(post): MDX引入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
@@ -99,6 +100,7 @@ export default defineConfig({
 		}),
 		svelte(),
 		sitemap(),
+		mdx({ optimize: true }),
 	],
 	markdown: {
 		remarkPlugins: [
