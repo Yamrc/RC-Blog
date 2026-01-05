8e26d6e282cfb8aa5c4adaf972f2f8b16954d79d
Yamrc <yamrci@outlook.com>
2026-01-05 21:19:35 +0800

    FIx(dom): label关联问题

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -158,8 +158,8 @@ $: if (initialized && keywordMobile) {
       dark:bg-white/5 dark:hover:bg-white/10 dark:focus-within:bg-white/10
 ">
     <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/30 dark:text-white/30" />
-	<label class="sr-only">Search</label>
-    <input placeholder="{i18n(I18nKey.search)}" type="search" bind:value={keywordDesktop} on:focus={() => search(keywordDesktop, true)}
+    <label for="search-input-desktop" class="sr-only">{i18n(I18nKey.search)}</label>
+    <input id="search-input-desktop" placeholder="{i18n(I18nKey.search)}" type="search" bind:value={keywordDesktop} on:focus={() => search(keywordDesktop, true)}
            class="transition-all pl-10 text-sm bg-transparent outline-0
          h-full w-40 active:w-60 focus:w-60 text-black/50 dark:text-white/50"
     >
@@ -188,8 +188,10 @@ $: if (initialized && keywordMobile) {
 				icon="material-symbols:search"
 				class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/30 dark:text-white/30"
 			/>
+			<label for="search-input-mobile" class="sr-only">{i18n(I18nKey.search)}</label>
 			<input
-				placeholder="Search"
+				id="search-input-mobile"
+				placeholder="{i18n(I18nKey.search)}"
 				bind:value={keywordMobile}
 				type="search"
 				class="pl-10 absolute inset-0 text-sm bg-transparent outline-0
